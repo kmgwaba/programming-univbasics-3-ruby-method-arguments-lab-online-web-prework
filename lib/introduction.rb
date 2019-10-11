7db4a6d7e557419ac65a33bfba9a9db3de4efd27
Kathia Mgwaba <mgwaba68@gmail.com>
2019-10-11 14:52:01 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,6 +6,11 @@ def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
+def introduction_with_language_optional(name, language)
+  "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
+
 
 
 
